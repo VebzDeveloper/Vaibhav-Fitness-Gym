e338c3524c0ff0b912f06a57255f6e5c1304238e
Vaibhav <58388765+VebzDeveloper@users.noreply.github.com>
2025-04-10 23:49:06 +0530

    Add files via upload

---
+++ js/timer.js
@@ -0,0 +1,22 @@
+// var countDownDate = new Date("jan 18, 2026 11:59:59").getTime();
+var countDownDate = new Date("april 10, 2025 11:59:59").getTime();
+
+var x = setInterval(function () {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
